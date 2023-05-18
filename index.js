86bcc29e5f97840c1e969f5dbec633f54d48826d
MD:Rubrl RM <inforubel251@gmail.com>
2023-05-18 23:30:28 +0600

    id to get api in daynamic

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -27,6 +27,22 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+
+        const animalsCollections = client.db('toysAnimals').collection('toys')
+
+        app.get('/AllToys', async (req, res) => {
+            const result = await animalsCollections.find().toArray();
+            res.send(result)
+        })
+
+        app.get('/details/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await animalsCollections.findOne(query)
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
